92d32f4bcc309f37f2c9299041e2756b45e4e4b8
Subhajit Chaudhury <subhajitchaudhury05@gmail.com>
2024-11-10 20:14:19 +0530

    recaped what i studied in part-1

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+function sum(a: number, b: number): number {
+  return a + b;
+}
+
+function greet(name: string): string {
+  return "Hello" + name;
+}
+
+function isEven(num: number): boolean {
+  if (num % 2 == 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
